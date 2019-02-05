7949ba41ec22d7e2a9097316d40933db4cc6975a
Arturs Klesuns <a.klesuns@dynatech.lv>
2019-02-05 21:17:49 +0200

    Update plugin.xml - 2019.02.05.002

--- src/org/klesun/deep_js_completion/entry/DeepJsCompletionConfidence.scala
+++ src/org/klesun/deep_js_completion/entry/DeepJsCompletionConfidence.scala
@@ -4,13 +4,15 @@ import com.intellij.codeInsight.completion.CompletionConfidence
 import com.intellij.psi.{PsiElement, PsiFile}
 import com.intellij.util.ThreeState
 import org.klesun.deep_js_completion.completion_providers.JsdocPvdr
+import org.klesun.lang.Lang
 
 class DeepJsCompletionConfidence extends CompletionConfidence {
   // JSAutoCompletionPopupPolicy specifically disables
   // completion auto-popup in doc comment body - have to override
   // that in order to see the require('<caret>') completion
-  override def shouldSkipAutopopup(contextElement: PsiElement, psiFile: PsiFile, offset: Int): ThreeState = {
-    if (JsdocPvdr.matchFileNameTaker(contextElement.getText).nonEmpty) {
+  override def shouldSkipAutopopup(psi: PsiElement, psiFile: PsiFile, offset: Int): ThreeState = {
+    val prefix = Lang.substr(psi.getText, 0, offset - psi.getTextOffset)
+    if (JsdocPvdr.matchFileNameTaker(prefix).nonEmpty) {
       ThreeState.NO
     } else {
       ThreeState.UNSURE
