1af5cd3c8427f52f26679580a9484caff4be51e1
Artur Klesun <arturklesun@gmail.com>
2019-08-17 16:42:42 +0000

    Correct indentation

--- src/org/klesun/deep_js_completion/resolvers/var_res/generic_res/ToGetTypeOfGeneric.scala
+++ src/org/klesun/deep_js_completion/resolvers/var_res/generic_res/ToGetTypeOfGeneric.scala
@@ -17,51 +17,56 @@ import org.klesun.lang.DeepJsLang._
 case class ToGetTypeOfGeneric(
   ctx: IExprCtx,
   generic: String,
-  generics: collection.mutable.Map[String, () => MemIt[JSType]],
+  genericsSrc: collection.mutable.Map[String, () => MemIt[JSType]],
 ) {
+  val generics = collection.mutable.Map[String, () => MemIt[JSType]]() ++= genericsSrc
+  generics.remove(generic)
+
   def applyToSingleType(
     sints: TypeScriptSingleType,
     getArgt: () => GenTraversableOnce[JSType],
   ): GenTraversableOnce[JSType] = {
-        val fqn = sints.getQualifiedTypeName
-        if (generic equals fqn) {
-          getArgt()
-        } else if ("Iterable" equals fqn) {
-          val getSubType = () => getArgt().itr.flatMap(t => ctx.mt().getKey(t, None))
-          sints.getTypeArguments.headOption.itr
-            .flatMap(eldec => apply(eldec, getSubType))
-        } else if ("ArrayIterator" equals fqn) {
-          // lodash; possibly should resolve these custom lib types rather than hardcode them...
-          // getArgt() holds the type of the passed function
-          val getArgMit = () => getArgt().mem()
-          val getArgType = () => {
-            // should probably store argument declaration types in deep js function as well... but not sure
-            None
-            //getArgMit().itr.flatMap(t => Mt.getArgType(t, ctx, 0))
-          }: GenTraversableOnce[JSType]
-          val getRetType = () => getArgMit().itr.flatMap(t => Mt.getReturnType(t, ctx))
-          cnc(
-            sints.getTypeArguments.lift(0).itr()
-              .flatMap(argdec => apply(argdec, getArgType)),
-            sints.getTypeArguments.lift(1).itr()
-              .flatMap(retdec => apply(retdec, getRetType)),
-          )
-        } else if (
-          ("PromiseLike" equals fqn) ||
-          ("Promise" equals fqn) ||
-          ("Bluebird" equals fqn)
-        ) {
-          val getSubType = () => getArgt().itr.flatMap(t => Mt.unwrapPromise(t))
-          sints.getTypeArguments.headOption.itr
-            .flatMap(eldec => apply(eldec, getSubType))
-        } else {
-          //Console.println("Unsupported generic type expr arg class - " + fqn + " - " + argTypePsi)
-          None
-        }
+    val fqn = sints.getQualifiedTypeName
+    if (generic equals fqn) {
+      getArgt()
+    } else if ("Iterable" equals fqn) {
+      val getSubType = () => getArgt().itr.flatMap(t => ctx.mt().getKey(t, None))
+      sints.getTypeArguments.headOption.itr
+        .flatMap(eldec => apply(eldec, getSubType))
+    } else if ("ArrayIterator" equals fqn) {
+      // lodash; possibly should resolve these custom lib types rather than hardcode them...
+      // getArgt() holds the type of the passed function
+      val getArgMit = () => getArgt().mem()
+      val getArgType = () => {
+        // should probably store argument declaration types in deep js function as well... but not sure
+        None
+        //getArgMit().itr.flatMap(t => Mt.getArgType(t, ctx, 0))
+      }: GenTraversableOnce[JSType]
+      val getRetType = () => getArgMit().itr.flatMap(t => Mt.getReturnType(t, ctx))
+      cnc(
+        sints.getTypeArguments.lift(0).itr()
+          .flatMap(argdec => apply(argdec, getArgType)),
+        sints.getTypeArguments.lift(1).itr()
+          .flatMap(retdec => apply(retdec, getRetType)),
+      )
+    } else if (
+      ("PromiseLike" equals fqn) ||
+      ("Promise" equals fqn) ||
+      ("Bluebird" equals fqn)
+    ) {
+      val getSubType = () => getArgt().itr.flatMap(t => Mt.unwrapPromise(t))
+      sints.getTypeArguments.headOption.itr
+        .flatMap(eldec => apply(eldec, getSubType))
+    } else {
+      //Console.println("Unsupported generic type expr arg class - " + fqn + " - " + sints)
+      None
+    }
   }
 
   /**
    * (arg: SomeCls<T>) => {} -> returns type of T, knowing the complete type of arg
+   *
+   * possibly can cause infinite recursion, should add check some time
    */
   def apply(
     argTypePsi: JSTypeDeclaration,
